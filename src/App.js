edcc92f8096c8b20fd4904fa427c25b645148a2f
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-25 19:47:23 +0100

    move form outside of header

--- src/App.js
+++ src/App.js
@@ -15,13 +15,13 @@ function App() {
         >
           Open Source Code by VHM
         </a>
-        <div className="city-weather">
-          <form>
-            <input type="search" placeholder="enter a city" />
-            <input type="submit" value="Search" />
-          </form>
-        </div>
       </header>
+      <div className="city-weather">
+        <form>
+          <input type="search" placeholder="enter a city" />
+          <input type="submit" value="Search" />
+        </form>
+      </div>
       <footer className="App-footer">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="text-gradient">Weather App</p>
