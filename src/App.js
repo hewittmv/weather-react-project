ee17c115e402c547a6b927783f724bd001f97f0c
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-25 16:36:40 +0100

    change header & footer h1 to <p>

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Weather App</h1>
+        <p>Weather App</p>
         <a
           className="github-link"
           href="https://github.com/hewittmv/weather-react-project"
@@ -18,7 +18,7 @@ function App() {
       </header>
       <footer className="App-footer">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Weather App</h1>
+        <p>Weather App</p>
         <a
           className="github-link"
           href="https://github.com/hewittmv/weather-react-project"
