ea2c583400e160fd8115ec57adf125327cc2a177
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-25 21:49:25 +0100

    Add text to weather app

--- src/App.js
+++ src/App.js
@@ -23,11 +23,15 @@ function App() {
               type="search"
               placeholder="enter a city"
               className="search-input"
-            />{" "}
-            {""}
+            />
             <input type="submit" value="Search" className="search-button" />
           </form>
           <button>📍</button>
+          <div>
+            <h1>New York</h1>
+            <p>29th Sept 21:00 </p>
+            <p>🌤️ 18°C Sunny </p>
+          </div>
         </div>
       </div>
       <footer className="App-footer">
