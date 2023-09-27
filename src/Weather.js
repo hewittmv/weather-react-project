9f96285172c87c1effff61262e2c4f343f95948a
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-27 17:01:56 +0100

    correct typo "date" > "data"

--- src/Weather.js
+++ src/Weather.js
@@ -23,7 +23,7 @@ export default function Weather(props) {
   }
 
   function Search() {
-    let apiKey = "o00a45f3c7757cta20b5ccabe4f8ba48";
+    const apiKey = "o00a45f3c7757cta20b5ccabe4f8ba48";
     let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showWeather);
   }
@@ -35,90 +35,6 @@ export default function Weather(props) {
   function SearchCity(event) {
     setCity(event.target.value);
   }
-
-  let blankForm = (
-    <div className="Weather">
-      <div className="city-weather">
-        <div className="container">
-          <form className="search-form">
-            <div className="row">
-              <div className="col-9">
-                <input
-                  type="search"
-                  placeholder="Enter a City"
-                  className="search-input"
-                  autoFocus
-                />
-              </div>
-              <div className="col-2">
-                <input
-                  type="submit"
-                  value="Search"
-                  className="search-button btn btn-primary"
-                />
-              </div>
-              <div className="col-1">
-                <button className="my-location">📍</button>
-              </div>
-            </div>
-          </form>
-        </div>
-        <div className="weather-data">
-          <div className="row">
-            <div className="col-8">
-              <h1> City </h1>
-              <p className="current-date-time">
-                {" "}
-                <em>loading date & time...</em>{" "}
-              </p>
-              <p className="weather-description">
-                <img
-                  src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-                  alt="broken clouds"
-                />
-                <span className="temperature"></span> ∽
-                <span className="unit">°C </span>
-              </p>
-              <p className="description">
-                {" "}
-                <em>Loading weather</em>{" "}
-              </p>
-            </div>
-            <div className="col-4">
-              <ul>
-                <li>
-                  <strong>Feels Like:</strong> ∽ °C
-                </li>
-                <li>
-                  <strong>Humidity:</strong> ∽%
-                </li>
-                <li>
-                  <strong>Wind:</strong> ∽ hm/h
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div className="forecast-data">
-          <div className="forecast-day">
-            <div>loading </div> <div>🌤️</div> <div>∽°C</div>
-          </div>
-          <div className="forecast-day">
-            <div>loading </div> <div>🌤️</div> <div>~°C</div>
-          </div>
-          <div className="forecast-day">
-            <div>loading </div> <div>☁️</div> <div>∽°C</div>
-          </div>
-          <div className="forecast-day">
-            <div>loading </div> <div>🌧️</div> <div>∽°C</div>
-          </div>
-          <div className="forecast-day">
-            <div>loading</div> <div>⛈️</div> <div>∽°C</div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
 
   if (weatherData.ready) {
     return (
@@ -149,13 +65,14 @@ export default function Weather(props) {
               </div>
             </form>
           </div>
-          <Condition date={weatherData} />
+          <Condition data={weatherData} />
 
           <Forecast city={weatherData.city} />
         </div>
       </div>
     );
   } else {
-    return blankForm;
+    Search();
+    return "Loading...";
   }
 }
