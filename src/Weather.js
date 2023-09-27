6036f43559da7b9e18d9004e571444ee33f3adda
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-27 14:43:25 +0100

    create apiKey variable

--- src/Weather.js
+++ src/Weather.js
@@ -22,7 +22,8 @@ export default function Weather() {
 
   function handleSubmit(event) {
     event.preventDefault();
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=o00a45f3c7757cta20b5ccabe4f8ba48&units=metric`;
+    let apiKey = "o00a45f3c7757cta20b5ccabe4f8ba48";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
     axios.get(apiUrl).then(showWeather);
   }
 
@@ -70,7 +71,7 @@ export default function Weather() {
                   src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
                   alt="broken clouds"
                 />
-                <span className="temperature">〰</span>
+                <span className="temperature"></span> ∽
                 <span className="unit">°C </span>
               </p>
               <p className="description">
