325949ee6ba66d41299e69cad6f57448b1a5ebeb
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-27 18:32:49 +0100

    add code to format date

---
+++ src/FormattedDate.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function FormattedDate(props) {
+  let weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let weekDay = weekDays[props.date.getDay()];
+  let dayOfMonth = props.date.getDate();
+  let hours = props.date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = props.data.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let month = months[props.date.getMonth()];
+
+  let year = props.date.getFullYear();
+  return (
+    <div className="FormattedDate">
+      {weekDay} wed
+      {dayOfMonth} 27
+      {month} sept
+      <div>
+        {hours}:{minutes} /{year}
+      </div>
+    </div>
+  );
+}
