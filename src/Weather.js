a0709fdb15e9292df5be2ae270e63e1c73ed1fbd
hewittmv <111692506+hewittmv@users.noreply.github.com>
2023-09-27 16:22:50 +0100

    import condition into weather component

--- src/Weather.js
+++ src/Weather.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
 import Forecast from "./Forecast";
+import Condition from "./Condition";
 
 export default function Weather() {
   const [weatherData, setWeatherData] = useState({ ready: false });
@@ -145,38 +146,8 @@ export default function Weather() {
               </div>
             </form>
           </div>
-          <div className="weather-data">
-            <div className="row">
-              <div className="col-8">
-                <h1>{weatherData.city}</h1>
-                <p className="current-date-time"> 29th Sept 23 17:15 </p>
-                <p className="weather-description">
-                  <img
-                    src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/broken-clouds-day.png"
-                    alt="broken clouds"
-                  />
-                  <span className="temperature">
-                    {weatherData.temperature}{" "}
-                  </span>
-                  <span className="unit">°C </span>
-                </p>
-                <p className="description">{weatherData.description}</p>
-              </div>
-              <div className="col-4">
-                <ul>
-                  <li>
-                    <strong>Feels Like:</strong> {weatherData.temperature}
-                  </li>
-                  <li>
-                    <strong>Humidity</strong> {weatherData.humidity}
-                  </li>
-                  <li>
-                    <strong>Wind:</strong> {weatherData.wind}hm/h
-                  </li>
-                </ul>
-              </div>
-            </div>
-          </div>
+          <Condition />
+
           <Forecast />
         </div>
       </div>
